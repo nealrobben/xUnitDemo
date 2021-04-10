a341978c5957b2141e30e2147d49252c4f07d016
Neal Robben <neal.robben@gmail.com>
2021-04-10 21:23:34 +0200

    Added new test project & gitignore file

---
+++ DemoProject/DemoProject.Main.Tests/Class1Tests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace DemoProject.Main.Tests
+{
+    public class Class1Tests
+    {
+        [Fact]
+        public void TestOne()
+        {
+            var sut = new Class1();
+            sut.FirstTest();
+        }
+
+        [Fact]
+        public void TestTwo()
+        {
+            var sut = new Class1();
+            sut.SecondTest();
+        }
+    }
+}
---
+++ DemoProject/DemoProject.Main/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace DemoProject.Main
+{
+    public class Class1
+    {
+        public void FirstTest()
+        {
+            Thread.Sleep(5000);
+        }
+
+        public void SecondTest()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
